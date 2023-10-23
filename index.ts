0318d0641d09690426f609b8fc883429f2ed49db
Josh Kodroff <josh@pulumi.com>
2023-10-23 18:09:31 -0400

    Replace with container template

--- index.ts
+++ index.ts
@@ -1,81 +1,111 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as azure_native from "@pulumi/azure-native";
-import * as synced_folder from "@pulumi/synced-folder";
+import * as resources from "@pulumi/azure-native/resources";
+import * as containerregistry from "@pulumi/azure-native/containerregistry";
+import * as containerinstance from "@pulumi/azure-native/containerinstance";
+import * as random from "@pulumi/random";
+import * as docker from "@pulumi/docker";
 
 // Import the program's configuration settings.
 const config = new pulumi.Config();
-const path = config.get("path") || "./www";
-const indexDocument = config.get("indexDocument") || "index.html";
-const errorDocument = config.get("errorDocument") || "error.html";
+const appPath = config.get("appPath") || "./app";
+const imageName = config.get("imageName") || "my-app";
+const imageTag = config.get("imageTag") || "latest";
+const containerPort = config.getNumber("containerPort") || 80;
+const cpu = config.getNumber("cpu") || 1;
+const memory = config.getNumber("memory") || 2;
 
-// Create a resource group for the website.
-// const resourceGroup = new azure_native.resources.ResourceGroup("resource-group", {});
+// Create a resource group for the container registry.
+const resourceGroup = new resources.ResourceGroup("resource-group");
 
-const resourceGroup = {
-    name: "resource-groupd53d754b"
-};
-
-// Create a blob storage account.
-const account = new azure_native.storage.StorageAccount("account", {
+// Create a container registry.
+const registry = new containerregistry.Registry("registry", {
     resourceGroupName: resourceGroup.name,
-    kind: "StorageV2",
+    adminUserEnabled: true,
     sku: {
-        name: "Standard_LRS",
+        name: containerregistry.SkuName.Basic,
     },
 });
 
-// Configure the storage account as a website.
-const website = new azure_native.storage.StorageAccountStaticWebsite("website", {
+// Fetch login credentials for the registry.
+const credentials = containerregistry.listRegistryCredentialsOutput({
     resourceGroupName: resourceGroup.name,
-    accountName: account.name,
-    indexDocument: indexDocument,
-    error404Document: errorDocument,
+    registryName: registry.name,
+}).apply(creds => {
+    return {
+        username: creds.username!,
+        password: creds.passwords![0].value!,
+    };
 });
 
-// Use a synced folder to manage the files of the website.
-const syncedFolder = new synced_folder.AzureBlobFolder("synced-folder", {
-    path: path,
-    resourceGroupName: resourceGroup.name,
-    storageAccountName: account.name,
-    containerName: website.containerName,
+// Create a container image for the service.
+const image = new docker.Image("image", {
+    imageName: pulumi.interpolate`${registry.loginServer}/${imageName}:${imageTag}`,
+    build: {
+        context: appPath,
+        platform: "linux/amd64",
+    },
+    registry: {
+        server: registry.loginServer,
+        username: credentials.username,
+        password: credentials.password,
+    },
 });
 
-// // Create a CDN profile.
-// const profile = new azure_native.cdn.Profile("profile", {
-//     resourceGroupName: resourceGroup.name,
-//     sku: {
-//         name: "Standard_Microsoft",
-//     },
-// });
+// Use a random string to give the service a unique DNS name.
+const dnsName = new random.RandomString("dns-name", {
+    length: 8,
+    special: false,
+}).result.apply(result => `${imageName}-${result.toLowerCase()}`);
 
-// // Pull the hostname out of the storage-account endpoint.
-// const originHostname = account.primaryEndpoints.apply(endpoints => new URL(endpoints.web)).hostname;
+// Create a container group for the service that makes it publicly accessible.
+const containerGroup = new containerinstance.ContainerGroup("container-group", {
+    resourceGroupName: resourceGroup.name,
+    osType: "linux",
+    restartPolicy: "always",
+    imageRegistryCredentials: [
+        {
+            server: registry.loginServer,
+            username: credentials.username,
+            password: credentials.password,
+        },
+    ],
+    containers: [
+        {
+            name: imageName,
+            image: image.imageName,
+            ports: [
+                {
+                    port: containerPort,
+                    protocol: "tcp",
+                },
+            ],
+            environmentVariables: [
+                {
+                    name: "PORT",
+                    value: containerPort.toString(),
+                },
+            ],
+            resources: {
+                requests: {
+                    cpu: cpu,
+                    memoryInGB: memory,
+                },
+            },
+        },
+    ],
+    ipAddress: {
+        type: containerinstance.ContainerGroupIpAddressType.Public,
+        dnsNameLabel: dnsName,
+        ports: [
+            {
+                port: containerPort,
+                protocol: "tcp",
+            },
+        ],
+    },
+});
 
-// // Create a CDN endpoint to distribute and cache the website.
-// const endpoint = new azure_native.cdn.Endpoint("endpoint", {
-//     resourceGroupName: resourceGroup.name,
-//     profileName: profile.name,
-//     isHttpAllowed: false,
-//     isHttpsAllowed: true,
-//     isCompressionEnabled: true,
-//     contentTypesToCompress: [
-//         "text/html",
-//         "text/css",
-//         "application/javascript",
-//         "application/json",
-//         "image/svg+xml",
-//         "font/woff",
-//         "font/woff2",
-//     ],
-//     originHostHeader: originHostname,
-//     origins: [{
-//         name: account.name,
-//         hostName: originHostname,
-//     }],
-// });
-
-// Export the URLs and hostnames of the storage account and CDN.
-export const originURL = account.primaryEndpoints.apply(endpoints => endpoints.web);
-// export { originHostname };
-// export const cdnURL = pulumi.interpolate`https://${endpoint.hostName}`;
-// export const cdnHostname = endpoint.hostName;
+// Export the service's IP address, hostname, and fully-qualified URL.
+export const hostname = containerGroup.ipAddress.apply(addr => addr!.fqdn!);
+export const ip = containerGroup.ipAddress.apply(addr => addr!.ip!);
+export const url = containerGroup.ipAddress.apply(addr => `http://${addr!.fqdn!}:${containerPort}`);
